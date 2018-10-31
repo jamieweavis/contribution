347fbdab409d91ca70694bf61e2c1a9f156894f9
Jamie Weavis <jamie.weavis@gmail.com>
2018-10-31 13:35:22 +0000

    Remove cors tests

--- test/contribution.test.js
+++ test/contribution.test.js
@@ -3,7 +3,7 @@ const contribution = require('../src/contribution');
 const validUsername = 'jamieweavis';
 const invalidUsername = 'hkwwezhsgyczzvjjktvvmneqxzidwupkyhtotanh';
 
-test('fetch contribution data via callback without CORS', done => {
+test('fetch contribution data via callback', done => {
   contribution(validUsername, {
     onSuccess: data => {
       expect(data).toHaveProperty('currentStreak');
@@ -17,22 +17,7 @@ test('fetch contribution data via callback without CORS', done => {
   });
 });
 
-test('fetch contribution data via callback with CORS', done => {
-  contribution(validUsername, {
-    enableCors: true,
-    onSuccess: data => {
-      expect(data).toHaveProperty('currentStreak');
-      expect(typeof data.currentStreak).toBe('number');
-      expect(data).toHaveProperty('bestStreak');
-      expect(typeof data.bestStreak).toBe('number');
-      expect(data).toHaveProperty('contributions');
-      expect(typeof data.contributions).toBe('number');
-      done();
-    }
-  });
-});
-
-test('fetch contribution data via promise without CORS', done => {
+test('fetch contribution data via promise', done => {
   contribution(validUsername).then(data => {
     expect(data).toHaveProperty('currentStreak');
     expect(typeof data.currentStreak).toBe('number');
@@ -44,19 +29,7 @@ test('fetch contribution data via promise without CORS', done => {
   });
 });
 
-test('fetch contribution data via promise with CORS', done => {
-  contribution(validUsername, { enableCors: true }).then(data => {
-    expect(data).toHaveProperty('currentStreak');
-    expect(typeof data.currentStreak).toBe('number');
-    expect(data).toHaveProperty('bestStreak');
-    expect(typeof data.bestStreak).toBe('number');
-    expect(data).toHaveProperty('contributions');
-    expect(typeof data.contributions).toBe('number');
-    done();
-  });
-});
-
-test('fetch contribution data via async/await without CORS', async done => {
+test('fetch contribution data via async/await', async done => {
   const data = await contribution(validUsername);
   expect(data).toHaveProperty('currentStreak');
   expect(typeof data.currentStreak).toBe('number');
@@ -67,18 +40,7 @@ test('fetch contribution data via async/await without CORS', async done => {
   done();
 });
 
-test('fetch contribution data via async/await with CORS', async done => {
-  const data = await contribution(validUsername, { enableCors: true });
-  expect(data).toHaveProperty('currentStreak');
-  expect(typeof data.currentStreak).toBe('number');
-  expect(data).toHaveProperty('bestStreak');
-  expect(typeof data.bestStreak).toBe('number');
-  expect(data).toHaveProperty('contributions');
-  expect(typeof data.contributions).toBe('number');
-  done();
-});
-
-test('fetch contribution data for invalid user via callback without CORS', done => {
+test('fetch contribution data for invalid user via callback', done => {
   contribution(invalidUsername, {
     onFailure: error => {
       expect(typeof error).toBe('object');
@@ -87,21 +49,20 @@ test('fetch contribution data for invalid user via callback without CORS', done 
       expect(error.statusCode).toEqual(404);
       done();
     }
-  })
+  });
 });
 
-test('fetch contribution data for invalid user via promise without CORS', done => {
-  contribution(invalidUsername)
-  .catch(error => {
+test('fetch contribution data for invalid user via promise', done => {
+  contribution(invalidUsername).catch(error => {
     expect(typeof error).toBe('object');
     expect(error).toHaveProperty('statusCode');
     expect(typeof error.statusCode).toBe('number');
     expect(error.statusCode).toEqual(404);
     done();
-  })
+  });
 });
 
-test('fetch contribution data for invalid user via async/await without CORS', async done => {
+test('fetch contribution data for invalid user via async/await', async done => {
   try {
     await contribution(invalidUsername);
   } catch (error) {
