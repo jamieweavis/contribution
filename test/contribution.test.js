22872eeb79a3216ae130a51e01e6340ada14cdb7
Jamie Weavis <jamie.weavis@gmail.com>
2019-01-09 11:42:33 +0000

    Simplify tests with reusable mock objects

--- test/contribution.test.js
+++ test/contribution.test.js
@@ -3,15 +3,19 @@ import contribution from '../lib';
 const validUsername = 'jamieweavis';
 const invalidUsername = 'veryveryveryveryveryverylonginvalidusername';
 
+const exampleData = {
+  currentStreak: expect.any(Number),
+  bestStreak: expect.any(Number),
+  contributions: expect.any(Number)
+};
+const exampleError = {
+  statusCode: expect.any(Number)
+};
+
 test('fetch contribution data via callback', done => {
   contribution(validUsername, {
     onSuccess: data => {
-      expect(data).toHaveProperty('currentStreak');
-      expect(typeof data.currentStreak).toBe('number');
-      expect(data).toHaveProperty('bestStreak');
-      expect(typeof data.bestStreak).toBe('number');
-      expect(data).toHaveProperty('contributions');
-      expect(typeof data.contributions).toBe('number');
+      expect(data).toEqual(expect.objectContaining(exampleData));
       done();
     }
   });
@@ -19,34 +23,21 @@ test('fetch contribution data via callback', done => {
 
 test('fetch contribution data via promise', done => {
   contribution(validUsername).then(data => {
-    expect(data).toHaveProperty('currentStreak');
-    expect(typeof data.currentStreak).toBe('number');
-    expect(data).toHaveProperty('bestStreak');
-    expect(typeof data.bestStreak).toBe('number');
-    expect(data).toHaveProperty('contributions');
-    expect(typeof data.contributions).toBe('number');
+    expect(data).toEqual(expect.objectContaining(exampleData));
     done();
   });
 });
 
 test('fetch contribution data via async/await', async done => {
   const data = await contribution(validUsername);
-  expect(data).toHaveProperty('currentStreak');
-  expect(typeof data.currentStreak).toBe('number');
-  expect(data).toHaveProperty('bestStreak');
-  expect(typeof data.bestStreak).toBe('number');
-  expect(data).toHaveProperty('contributions');
-  expect(typeof data.contributions).toBe('number');
+  expect(data).toEqual(expect.objectContaining(exampleData));
   done();
 });
 
-test('fetch contribution data for invalid user via callback', done => {
+test('fetch contribution data f invalid user via callback', done => {
   contribution(invalidUsername, {
     onFailure: error => {
-      expect(typeof error).toBe('object');
-      expect(error).toHaveProperty('statusCode');
-      expect(typeof error.statusCode).toBe('number');
-      expect(error.statusCode).toEqual(404);
+      expect(error).toEqual(expect.objectContaining(exampleError));
       done();
     }
   });
@@ -54,10 +45,7 @@ test('fetch contribution data for invalid user via callback', done => {
 
 test('fetch contribution data for invalid user via promise', done => {
   contribution(invalidUsername).catch(error => {
-    expect(typeof error).toBe('object');
-    expect(error).toHaveProperty('statusCode');
-    expect(typeof error.statusCode).toBe('number');
-    expect(error.statusCode).toEqual(404);
+    expect(error).toEqual(expect.objectContaining(exampleError));
     done();
   });
 });
@@ -66,10 +54,7 @@ test('fetch contribution data for invalid user via async/await', async done => {
   try {
     await contribution(invalidUsername);
   } catch (error) {
-    expect(typeof error).toBe('object');
-    expect(error).toHaveProperty('statusCode');
-    expect(typeof error.statusCode).toBe('number');
-    expect(error.statusCode).toEqual(404);
+    expect(error).toEqual(expect.objectContaining(exampleError));
     done();
   }
 });
