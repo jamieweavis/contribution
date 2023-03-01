19db5b61fe6cd7a0e395ea87d031f41b4458b90b
Jamie Weavis <jamie.weavis@gmail.com>
2023-03-01 08:34:59 +0000

    chore: upgrade & update tooling

--- src/contribution.spec.ts
+++ src/contribution.spec.ts
@@ -1,4 +1,4 @@
-import { fetchStats } from './index';
+import { fetchStats } from './contribution';
 
 const validUsername = 'jamieweavis';
 const invalidUsername = 'veryveryveryveryveryverylonginvalidusername';
@@ -18,50 +18,50 @@ const errorShape = {
   statusCode: expect.any(Number),
 };
 
-describe('contribution', (): void => {
-  describe('the fetchStats function', (): void => {
-    describe('when provided with a valid username', (): void => {
-      it('should execute the success callback with contribution data', (done): void => {
+describe('contribution', () => {
+  describe('the fetchStats function', () => {
+    describe('when provided with a valid username', () => {
+      it('should execute the success callback with contribution data', (done) => {
         fetchStats(validUsername, {
-          onSuccess: (stats): void => {
+          onSuccess: (stats) => {
             expect(stats).toEqual(expect.objectContaining(responseShape));
             done();
           },
         });
       });
 
-      it('should resolve a promise with contribution data', (done): void => {
-        fetchStats(validUsername).then((data): void => {
+      it('should resolve a promise with contribution data', (done) => {
+        fetchStats(validUsername).then((data) => {
           expect(data).toEqual(expect.objectContaining(responseShape));
           done();
         });
       });
 
-      it('should await with contribution data', async (done): Promise<void> => {
+      it('should await with contribution data', async (done) => {
         const data = await fetchStats(validUsername);
         expect(data).toEqual(expect.objectContaining(responseShape));
         done();
       });
     });
 
-    describe('when provided with an invalid username', (): void => {
-      it('should execute the failure callback with error message', (done): void => {
+    describe('when provided with an invalid username', () => {
+      it('should execute the failure callback with error message', (done) => {
         fetchStats(invalidUsername, {
-          onFailure: (error): void => {
+          onFailure: (error) => {
             expect(error).toEqual(expect.objectContaining(errorShape));
             done();
           },
         });
       });
 
-      it('should catch the promise rejection with error message', (done): void => {
-        fetchStats(invalidUsername).catch((error): void => {
+      it('should catch the promise rejection with error message', (done) => {
+        fetchStats(invalidUsername).catch((error) => {
           expect(error).toEqual(expect.objectContaining(errorShape));
           done();
         });
       });
 
-      it('should catch the error with error message', async (done): Promise<void> => {
+      it('should catch the error with error message', async (done) => {
         try {
           await fetchStats(invalidUsername);
         } catch (error) {
