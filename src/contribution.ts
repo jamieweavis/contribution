acfb0399e1b849e670a40ac2caedbd66633d9d71
Jamo <jamie.weavis@gmail.com>
2024-11-19 13:22:23 +0000

    style: run formatter

--- src/contribution.ts
+++ src/contribution.ts
@@ -1,41 +1,41 @@
-import type { IncomingMessage } from "node:http";
-import { get } from "node:https";
+import type { IncomingMessage } from 'node:http';
+import { get } from 'node:https';
 
 import {
-	GitHubStats,
-	parseContributions,
-	parseGitHubStats,
-} from "./transformers";
+  GitHubStats,
+  parseContributions,
+  parseGitHubStats,
+} from './transformers';
 
 interface Options {
-	onSuccess?: (stats: GitHubStats) => unknown;
-	onFailure?: (error: IncomingMessage) => unknown;
+  onSuccess?: (stats: GitHubStats) => unknown;
+  onFailure?: (error: IncomingMessage) => unknown;
 }
 
 const fetchStats = (
-	username: string,
-	options?: Options,
+  username: string,
+  options?: Options,
 ): Promise<GitHubStats> =>
-	new Promise((resolve, reject) => {
-		get(`https://github.com/users/${username}/contributions`, (response) => {
-			let body = "";
-			response.setEncoding("utf8");
-			response.on("data", (chunk) => {
-				body += chunk;
-			});
-			response.on("end", () => {
-				if (response.statusCode === 404) {
-					if (options?.onFailure) return options.onFailure(response);
-					return reject(response);
-				}
+  new Promise((resolve, reject) => {
+    get(`https://github.com/users/${username}/contributions`, (response) => {
+      let body = '';
+      response.setEncoding('utf8');
+      response.on('data', (chunk) => {
+        body += chunk;
+      });
+      response.on('end', () => {
+        if (response.statusCode === 404) {
+          if (options?.onFailure) return options.onFailure(response);
+          return reject(response);
+        }
 
-				const contributions = parseContributions(body);
-				const gitHubStats = parseGitHubStats(contributions);
+        const contributions = parseContributions(body);
+        const gitHubStats = parseGitHubStats(contributions);
 
-				if (options?.onSuccess) return options.onSuccess(gitHubStats);
-				return resolve(gitHubStats);
-			});
-		});
-	});
+        if (options?.onSuccess) return options.onSuccess(gitHubStats);
+        return resolve(gitHubStats);
+      });
+    });
+  });
 
 export { GitHubStats, fetchStats };
